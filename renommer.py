aac442ddc5c062ed4f5bd734e4057b4ba8ba8ffe
sedhadcci <125959892+sedhadcci@users.noreply.github.com>
2023-10-04 16:00:28 +0200

    Update renommer.py

--- renommer.py
+++ renommer.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import os
 import shutil
-from zipfile import ZipFile
 import tempfile
 
 def rename_files(folder_path, new_prefix):
@@ -17,18 +16,21 @@ st.title("Renommage des fichiers Excel")
 
 uploaded_files = st.file_uploader("Téléchargez les fichiers Excel", accept_multiple_files=True)
 
-new_prefix = st.text_input("Nouvelle préfixe:", value="22-EDF")
+new_prefix = st.text_input("Nouvelle préfixe:", value="51-EMD-SCHINDLER")
 
 if uploaded_files and new_prefix:
     with tempfile.TemporaryDirectory() as temp_dir:
         for uploaded_file in uploaded_files:
-            with open(os.path.join(temp_dir, uploaded_file.name), "wb") as f:
+            file_path = os.path.join(temp_dir, uploaded_file.name)
+            with open(file_path, "wb") as f:
                 f.write(uploaded_file.getbuffer())
         rename_files(temp_dir, new_prefix)
         # Créer un fichier zip pour le dossier modifié
         shutil.make_archive("renamed_files", 'zip', temp_dir)
+        with open("renamed_files.zip", "rb") as f:
+            bytes = f.read()
         st.download_button(
             label="Téléchargez le dossier renommé",
-            data=open("renamed_files.zip", "rb"),
+            data=bytes,
             file_name="renamed_files.zip"
         )
